b4a4c4e0da7a9cde8329ce1779baf6d1a70388ee
Lixívia <38163500+LixiviaDev@users.noreply.github.com>
2025-06-16 11:46:11 +0000

    Added simple style to the cards!

--- src/components/retail/itemCard.tsx
+++ src/components/retail/itemCard.tsx
@@ -6,8 +6,15 @@ export default function itemCard(props: any) {
     const [itemData] = useState<ItemData>(props.itemData);
     
     return (
-        <div>
-            This is the card nº{itemData.id}!
+        <div className="m-5 p-5 flex justify-between border-2 border-solid rounded-lg shadow-xl">
+            <div className='flex flex-col gap-3'>
+                <div>{itemData.name}</div>
+                <div>{itemData.category}</div>
+            </div>
+            <div className='flex flex-col gap-3 text-right'> 
+                <div>{(itemData.discountedSellingPrice / 100).toFixed(2)}€ x {itemData.quantity}u.</div>
+                <div>{itemData.weightInGms}mg</div>
+            </div>
         </div>
     )
 }
